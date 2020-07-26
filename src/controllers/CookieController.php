80643d5f88af1c70dad7ecd3962da2a93f13ddbc
Quentin HAETTEL <quentin.haettel@gmail.com>
2020-07-26 14:48:19 +0200

    Ajout SameSite

--- src/controllers/CookieController.php
+++ src/controllers/CookieController.php
@@ -5,7 +5,20 @@ require_once '../entities/Cookie.php';
 class CookieController {
 
     public function createCookie($cookie) {
-        setcookie($cookie->getName(), $cookie->getValue(), $cookie->getTime(), $cookie->getPath(), $cookie->getDomain(), $cookie->getSecure(), $cookie->getHttponly());
+
+        if (PHP_VERSION_ID > 70400) {
+            setcookie($cookie->getName(), $cookie->getValue(), [
+                'expires' => $cookie->getTime(),
+                'path' => $cookie->getPath(),
+                'domain' => $cookie->getDomain(),
+                'samesite' => $cookie->getSamesite(),
+                'secure' => $cookie->getSecure(),
+                'httponly' => $cookie->getHttponly()
+            ]);
+        }
+        else {
+            setcookie($cookie->getName(), $cookie->getValue(), $cookie->getTime(), "'".$cookie->getPath()."; samesite=".$cookie->getSamesite()."'", $cookie->getDomain(), $cookie->getSecure(), $cookie->getHttponly());
+        }
     }
 
 }
